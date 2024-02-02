fb7b4a314e50e6a6f6d1cac06229133d918ea4b6
nooburger81 <brandonsebastian@gmail.com>
2024-02-02 15:12:00 -0500

    getting site deployed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -99,7 +99,7 @@ const Navbar = () => {
         </ul>
         
         {/* Social icons */}
-        <div className='hidden lg:flex fixed flex-col top-[35%] left-0 animate__animated animate__slideInLeft animate__delay-7s'>
+        <div className='hidden lg:flex fixed flex-col top-[20%] left-0 animate__animated animate__slideInLeft animate__delay-7s'>
             <ul>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a

e6714b26fe868bc96158a8494c6659d841465ea9
nooburger81 <brandonsebastian@gmail.com>
2024-02-01 21:47:47 -0500

    navbar made

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import {
+    FaBars,
+    FaTimes,
+  } from 'react-icons/fa';
+  import Logo from '../assets/SeabassStudioLogo2.png';
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#c0d6e4] text-black-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#fdfdfd] text-[#102E4A]'>
+        <div>
+            <img src={Logo} alt='Logo Image' style={{ width: '100px'}} />
+        </div>
         
+        {/*menu */}
+            <ul className='hidden md:flex'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+        
+
+          {/* Hamburger */}
+          <div className='hidden'>
+            <FaBars />
+          </div>
+
+        {/* Mobile menu */}
+        <ul className='hidden'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact</li>
+        </ul>
+        
+        {/* Social icons */}
+        <div></div>
     </div>
   )
 }

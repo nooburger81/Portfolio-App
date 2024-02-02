6e81c3223c39f405508dc16a41c7b7cc2b265c89
nooburger81 <brandonsebastian@gmail.com>
2024-02-01 19:37:15 -0500

    getting tailwind right, hehe

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#c0d6e4] text-black-300'>
+        
+    </div>
+  )
+}
+
+export default Navbar
